2f3a847c4459617941782354d6fb3f25ded44280
jeanfrancoisdrapeau <eluder25rs@gmail.com>
2017-06-18 00:33:13 -0400

    Test2

--- subpine.py
+++ subpine.py
@@ -1,60 +1,117 @@
-import pygame, sys, os, time, datetime, urllib
+import pygame, sys, time, datetime, obd
 from pygame.locals import *
+from obd import OBDStatus
 
-pygame.init()
-clock = pygame.time.Clock()
+port = ""
+baud = 115200
+protocol = "5"
 
-# Set up the screen
+connection = None
 
-DISPLAYSURF = pygame.display.set_mode((320, 240), 0, 16)
-pygame.mouse.set_visible(0)
-pygame.display.set_caption('Stats')
+rpmString = "N/A"
 
-# set up the colors
-BLACK = (  0,   0,   0)
-WHITE = (255, 255, 255)
-RED   = (255,   0,   0)
-GREEN = (  0, 255,   0)
-BLUE  = (  0,   0, 255)
-CYAN  = (  0, 255, 255)
+def new_rpm(r):
+    global rpmString
+    rpmString = str(r.value.magnitude)
 
-# Main loop
+def connect(p, b, pr):
+    global connection
+    s = "Unknown"
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+    try:
+        connection = obd.Async(p, b, pr)
+        connection.watch(obd.commands.RPM, callback=new_rpm)
+        connection.start()
 
-    currentime = datetime.datetime.time(datetime.datetime.now())
+        s = "Connected"
+    except:
+        s = "Not connected"
 
-    # Draw the title
+    return s
 
-    black_square_that_is_the_size_of_the_screen = pygame.Surface(DISPLAYSURF.get_size())
-    black_square_that_is_the_size_of_the_screen.fill((0, 0, 0))
-    DISPLAYSURF.blit(black_square_that_is_the_size_of_the_screen, (0, 0))
+if __name__ == "__main__":
+    pygame.init()
+    clock = pygame.time.Clock()
 
-    font = pygame.font.Font(None, 40)
-    text = font.render("Outside", 1, RED)
-    textpos = text.get_rect(centerx=DISPLAYSURF.get_width()/4+13)
-    DISPLAYSURF.blit(text, textpos)
+    doConnect = True
+    lastDoConnect = time.time()
 
-    font = pygame.font.Font(None, 40)
-    text = font.render("Inside", 1, RED)
-    textpos = text.get_rect(centerx=DISPLAYSURF.get_width()/4+179)
-    DISPLAYSURF.blit(text, textpos)
+    # Set up the screen
 
-    # Draw Lines
+    DISPLAYSURF = pygame.display.set_mode((800, 480), 0, 16)
+    pygame.mouse.set_visible(0)
+    pygame.display.set_caption('SubPINE')
 
-    pygame.draw.line(DISPLAYSURF, GREEN, [5, 140], [DISPLAYSURF.get_width()-5, 140], 1)
+    # set up the colors
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    CYAN = (0, 255, 255)
 
-    pygame.draw.line(DISPLAYSURF, GREEN, [DISPLAYSURF.get_width()/2+30, 5], [DISPLAYSURF.get_width()/2+30, 140], 1)
+    # Main loop
+    while True:
+        status = "Not connected"
+        if doConnect:
+            doConnect = False
+            lastDoConnect = time.time()
+            if connection is None:
+                status = connect(port, baud, protocol)
+            elif connection.status() == OBDStatus.NOT_CONNECTED:
+                status = connect(port, baud, protocol)
 
-    clock.tick()
-    font = pygame.font.Font(None, 75)
-    text = font.render("{0:.2f} fps".format(clock.get_fps()), 1, CYAN)
-    textpos = text.get_rect(center=(DISPLAYSURF.get_width() / 2, 215))
-    DISPLAYSURF.blit(text, textpos)
+        if (time.time() - lastDoConnect) > 2:
+            doConnect = True
 
-    pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
 
+        currentime = datetime.datetime.time(datetime.datetime.now())
+
+        # Draw the title
+        black_square_that_is_the_size_of_the_screen = pygame.Surface(DISPLAYSURF.get_size())
+        black_square_that_is_the_size_of_the_screen.fill((0, 0, 0))
+        DISPLAYSURF.blit(black_square_that_is_the_size_of_the_screen, (0, 0))
+
+        font = pygame.font.Font(None, 60)
+        text = font.render("RPM: " + str(rpmString), 1, RED)
+
+        DISPLAYSURF.blit(text, (0, 0))
+
+        # textstring = "N/A"
+        # if connection is not None:
+        #     try:
+        #         obdvalue1 = connection.query(obd.commands.O2_S1_WR_VOLTAGE)
+        #         textstring = obdvalue1.value.magnitude / 14.7
+        #     except:
+        #         connection.close()
+        # font = pygame.font.Font(None, 60)
+        # text = font.render("AFR: " + str(textstring), 1, RED)
+        # DISPLAYSURF.blit(text, (0, 60))
+
+        # Draw Lines
+
+        # pygame.draw.line(DISPLAYSURF, GREEN, [5, 140], [DISPLAYSURF.get_width()-5, 140], 1)
+
+        # Status
+        font = pygame.font.Font(None, 40)
+        text = font.render(status, 1, CYAN)
+        pygame.draw.line(DISPLAYSURF, GREEN, [1, DISPLAYSURF.get_height() - text.get_height()],
+                         [DISPLAYSURF.get_width(), DISPLAYSURF.get_height() - text.get_height()], 1)
+        DISPLAYSURF.blit(text, (1, DISPLAYSURF.get_height() - text.get_height()))
+
+        # FPS
+        clock.tick()
+        font = pygame.font.Font(None, 40)
+        text = font.render("{0:.2f} fps".format(clock.get_fps()), 1, CYAN)
+        DISPLAYSURF.blit(text, (DISPLAYSURF.get_width() - text.get_width(),
+                                DISPLAYSURF.get_height() - text.get_height()))
+
+        pygame.display.update()
+
+    if connection is not None:
+        connection.close()
+    sys.exit()
